2d15245cebc034e0f6e6dac23fad91398505bffd
ana <lavanyasukumar10@gmail.com>
2018-04-29 17:00:24 -0700

    Added a comment

--- CRUDWithJPA/CRUDWithJPAInSpringBoot/src/main/java/com/example/demo/CrudWithJpaInSpringBootApplication.java
+++ CRUDWithJPA/CRUDWithJPAInSpringBoot/src/main/java/com/example/demo/CrudWithJpaInSpringBootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudWithJpaInSpringBootApplication {
 
+	// Get your hands working
 	public static void main(String[] args) {
 		SpringApplication.run(CrudWithJpaInSpringBootApplication.class, args);
 	}
